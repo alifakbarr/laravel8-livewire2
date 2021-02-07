e1490ef01c24b2900a8fc4472a30b58a06d8c848
alifakbarr <alifakbarr8800@gmail.com>
2021-02-07 20:44:07 +0700

    membuat create

--- app/Http/Livewire/User.php
+++ app/Http/Livewire/User.php
@@ -7,10 +7,56 @@ use App\Models\User as tableUser;
 class User extends Component
 {
     public $users;
+
+    public $name;
+    public $email;
+    public $password;
+    // aturan penulisan yang diinginkan
+    protected $rules = [
+        'name' => 'required',
+        'email' => 'required|email|unique:users,email',
+        'password'=> 'required|min:4',
+    ];
+
+    // pesan error yang muncul
+    protected $messages = [
+        'name.required'=> 'Masukkan nama',
+        'email.required' => 'Masukkan Email',
+        'email.email' => 'Format email salah',
+        'email.unique' => 'Email sudah ada',
+        'password.required' => 'Masukkan password',
+        'password.min' => 'Min 4 char',
+
+    ];
+
+    public function ClearForm(){
+        $this->name='';
+        $this->email='';
+        $this->password='';
+    }
+
+    //real-time cek form 
+    public function updated($field)
+    {
+        $this->validateOnly($field);
+    }
+
+    // menyimpan data
+    public function SimpanData()
+    {
+        $validasi =$this->validate();
+
+        tableUser::create($validasi);
+        session()->flash('pesan','Data berhasil Disimpan');
+        $this->ClearForm();
+        // agar auto close modal
+        $this->emit('addData');
+    }
+
     public function render()
     {
         // manampilkan semua data user
-        $this->users=tableUser::all();
+        $this->users=tableUser::orderBy('id','DESC')->get();
         return view('livewire.user')
         // memasukkan template
         ->extends('layouts.app')
--- resources/views/livewire/user.blade.php
+++ resources/views/livewire/user.blade.php
@@ -4,7 +4,14 @@
     <div class="card">
         <div class="card-header">
           Data User
+          <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addData">
+            Tambah
+          </button>
         </div>
+        
+        @if (session('pesan'))
+            {{ session('pesan') }}
+        @endif
         <div class="card-body">
             <div class="table-responsive">
                 <table class="table">
@@ -32,4 +39,45 @@
             </div>
         </div>
       </div>
+      <!-- Modal Add Data-->
+      {{-- wire:ignore.self agar saat di submit ngeclose --}}
+        <div wire:ignore.self class="modal fade" id="addData" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Tambah Data</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                <form>
+                  <div class="mb-3">
+                    <label for="name" class="form-label">Name</label>
+                    <input name="name" wire:model="name" class="form-control" id="name">
+                    @error('name') <label class="text-danger">{{ $message }}</label> @enderror
+                  </div>
+                  <div class="mb-3">
+                    <label for="email" class="form-label">Email</label>
+                    <input name="email" wire:model="email" class="form-control" id="email">
+                    @error('email') <label class="text-danger">{{ $message }}</label> @enderror
+                  </div>
+                  <div class="mb-3">
+                    <label for="password" class="form-label">password</label>
+                    <input name="password" type="password" wire:model="password" class="form-control" id="password">
+                    @error('password') <label class="text-danger">{{ $message }}</label> @enderror
+                  </div>
+                  
+                </form>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary" wire:click.prevent="SimpanData()">Simpan</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <script>
+          window.livewire.on('addData',()=>{
+            $('#addData').modal('hide');
+          });
+        </script>
 </div>
